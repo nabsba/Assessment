79a7a6b9484a8cca738659461a65501a1b8f787f
nabsba <ns-dev@outlook.com>
2026-01-12 21:48:17 +0100

    fix-style(MainSearchGithubName.tsx): respect scrolling rule

--- src/features/searchGithubName/components/MainSearchGitHubName.tsx
+++ src/features/searchGithubName/components/MainSearchGitHubName.tsx
@@ -1,28 +1,45 @@
 import Header from './header/Header'
 import ListCards from './listCards/ListCards'
 import Menus from './menus/Menus'
-import styles from './mainSearch.module.css';  
+import styles from './mainSearch.module.css';
 import { ErrorType } from '../../error/components/ErrorComponent'
 import { LoaderType } from '../../loader/components/Loader'
 import DataFetchingWrapper from '../../shared/components/DataFetchingWrapper'
 import { useSearchContext } from '../hooks/GitHubContext'
 import GitHubSearch from './GitHubSearchInput/GitHubSearchInput'
+
 export default function MainSearchGitHubName() {
-
-
   const { state } = useSearchContext();
 
   return (
     <>
-      <Header />
-      <div className={styles.mainContainer}> 
-      <DataFetchingWrapper isLoading={state.loading} isError={!!state.error} errorProps={{ type: ErrorType.SERVER }} loaderProps={{ type: LoaderType.DOTS }} >
-        <div className={`flex-center ${styles.searchContainer}`}>
-          <GitHubSearch />
+      {/* Header fixed at top */}
+      <div className={styles.fixedHeader}>
+        <Header />
+      </div>
+
+      <div className={styles.mainContainer}>
+        {/* Search and menus - sticky below header */}
+        <div className={styles.stickySection}>
+          <div className={`flex-center ${styles.searchContainer}`}>
+            <GitHubSearch />
+          </div>
+          <div className={styles.menusContainer}>
+            <Menus />
+          </div>
         </div>
-        <div className={styles.menusContainer}> <Menus /></div>
-        <ListCards />
-      </DataFetchingWrapper>
+
+        {/* Scrollable content area */}
+        <div className={styles.scrollableContent}>
+          <DataFetchingWrapper
+            isLoading={state.loading}
+            isError={!!state.error}
+            errorProps={{ type: ErrorType.SERVER }}
+            loaderProps={{ type: LoaderType.DOTS }}
+          >
+            <ListCards />
+          </DataFetchingWrapper>
+        </div>
       </div>
     </>
   )
